bb53147daa7b2d8c3cfa586edd869e2252ae51ba
TC999999 <130585978+TC999999@users.noreply.github.com>
2023-04-20 18:13:25 -0400

    Update HelpTool_class.cpp

--- HelpTool_class.cpp
+++ HelpTool_class.cpp
@@ -6,7 +6,7 @@ class Helptool{
   string initial_request = "Would you like to be a player or a judge?";
   string player = "You are now a player. Convince the judge that you are a human and not an AI by answering their questions";
   string judge = "You are now a judge. Ask questions to both a human player and an AI and figure out which is the human";
-  string wait = "Please wait to be paired with another number";
+  string waitlist = "Please wait to be paired with another number";
   string pair = "You have been paired. Time to begin!";
 }
 public:
@@ -23,7 +23,7 @@ string getJudge(){
 }
 
 string getWait(){
-  return wait;
+  return waitlist;
 }
 
 string getPair()
