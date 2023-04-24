8b150ed52dc4a775d34674b6c8c10a71c70a54b9
TC999999 <130585978+TC999999@users.noreply.github.com>
2023-04-24 12:20:46 -0400

    Update Delegator.h

--- Delegator.h
+++ Delegator.h
@@ -6,53 +6,53 @@ using namespace std;
 class Delegator{
         queue<string> p_num;
         queue<string> j_num;
+        set<string> asked_player_judge;
         Gamebook* GB;
         AI* ai;
         Helptool* help;
         Twilio* twilio;
 
         public:
-        void addPlayerID() {
-                p_num.push(player.getPhoneNumber());
-        }
-
-        void addJudgeID() {
-                j_num.push(judge.getPhoneNumber());
-        }
-
         void analyze_request(string message, string number){
-                if (message == "Player"){
-                        addPlayerID();
-                        tool.getPlayer();
+                asked_player_judge.push(number);
+                Game *g = GB->inGame(number);
+                if (g != NULL){
+                        if(g->findRole(player) == "Judge"){  
+                            if(   
+                        }
+                        
+                        if(g->findRole(player) == "Player"){
+                             //we know this person is a player
+                             // this player has a responded to a judge's question, so we need to send the sms to twilio
+                             twilio -> send_message();
+                             
+                        }
+                   }
                 }
-                if (message == "Judge"){
-                        addJudgeID();
-                        tool.getJudge();
+                
+                if (asked_player_judge.contains(number)){
+                        if (message == "Player"){
+                                p_num.push(number);
+                                help->getPlayer();
+                        }
+                        
+                        if (message == "Judge"){
+                                j_num.push(number);
+                                help->getJudge();  
+                        }
+                        asked_player_judge.erase(number)
+                }
+                else{
+                     help->error();
+                     help->instructions();
                 }
         }
 
         void pair_human_agent_evaluator(string p_num, string j_num){
-                tool.getPair();
+                help->getPair();
                 g = new Game(string p_num, string j_num);
-                gamebook->addGame(g);
+                GB->addGame(g);
                 g.start();
         }
         
-        bool pairWaiting(){
-                if (!get_p_num().empty() && !get_j_num().empty()){
-                        return true;    
-                }
-                else{
-                        return false;
-                }
-        }
-        
-        bool is_game_question(string message){
-               if (message -> j_num){
-                       return true;
-               }
-               else{
-                       return false;
-               }
-        }
 };
