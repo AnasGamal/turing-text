5f798c0489f466b428a0a568d07f10d5f4c88aff
TC999999 <130585978+TC999999@users.noreply.github.com>
2023-04-24 16:48:49 -0400

    Update Delegator.h

--- Delegator.h
+++ Delegator.h
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <set>
 using namespace std;
 
 class Delegator{
@@ -17,7 +18,7 @@ class Delegator{
                 asked_player_judge.push(number);
                 Game *g = GB->inGame(number);
                 if (g != NULL){
-                        if(g->findRole(player) == "Judge"){  
+                        if(g->findRole(number) == "Judge"){  
                             //we know that this person is a judge
                             //the judge asks either the ai or the player a question
                             //we need to somehow determine whether the judge is either messaging the player or the ai
@@ -27,7 +28,7 @@ class Delegator{
                             // if the message contains player a in it and if player a is assigned to the ai, then we send the question to the ai
                             // if the message contains player b in it and if player b is assigned to the ai, then we send the question to the ai
                             if(message.find("Player A") || message.find("player A") || message.find("Player a") || message.find("player a")){
-                                if(Player = A){//however Jeremy and Rayan assign the player to either A or B in game class
+                                if(p_num = "A"){//however Jeremy and Rayan assign the player to either A or B in game class
                                         twilio -> send_sms(p_num, message);
                                 }
                                 else{
@@ -36,7 +37,7 @@ class Delegator{
                             }
                             
                             if(message.find("Player B") || message.find("player B") || message.find("Player b") || message.find("player b")){
-                                if(Player = B){//however Jeremy and Rayan assign the player to either A or B in game class
+                                if(p_num = "B"){//however Jeremy and Rayan assign the player to either A or B in game class
                                         twilio -> send_sms(p_num, message);
                                     }
                                 else{
@@ -45,7 +46,7 @@ class Delegator{
                             }
                         }
                 
-                        if(g->findRole(player) == "Player"){
+                        if(g->findRole(number) == "Player"){
                              //we know this person is a player
                              // this player has a responded to a judge's question, so we need to send the sms to twilio
                              twilio -> send_sms(j_num, message);
@@ -55,26 +56,26 @@ class Delegator{
                 if (asked_player_judge.contains(number)){
                         if (message.find("Player") || message.find("player")){
                                 p_num.push(number);
-                                help->getPlayer();
+                                help -> getPlayer();
                         }
                         
                         if (message.find("Judge") || message.find("judge")){
                                 j_num.push(number);
-                                help->getJudge();  
+                                help -> getJudge();  
                         }
                         asked_player_judge.erase(number)
                 }
                 else{
-                     help->error();
-                     help->instructions();
+                     help -> error();
+                     help -> instructions();
                 }
         }
 
         void pair_human_agent_evaluator(string p_num, string j_num){
-                help->getPair();
-                g = new Game(string p_num, string j_num);
-                GB->addGame(g);
-                g.start();
+                help -> getPair();
+                g = new Game(p_num, j_num);
+                GB -> addGame(g);
+                g->start();
         }
         
 };
