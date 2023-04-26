f7b833ebe33819b884077e59936a98beda6e24d9
Skyler Gipson <smgipson84@gmail.com>
2023-04-26 15:39:38 +0000

    content update

--- Human.h
+++ Human.h
@@ -1,42 +1,44 @@
-#include <string>
-using namespace std;
+#ifndef HUMAN_H 
+#define HUMAN_H
 
-class Human {
-	string phoneNumber;
-	string role;
+//class Human {
+	/*string role;
 	string accountSID;
 	string authToken;
-	string twilioNum;
+	string twilioNum;*/
 
-	public:
-	void setPhoneNumber(string phoneNumber) {
-		this ->phoneNumber = phoneNumber;
-	}
-	string getPhoneNumber() {
-		return phoneNumber;
-	}
-	void setRole(string role) {
-		this ->role = role;
-	}
-	string getRole() {
-		return role;
-	}
-	void setAccountSID(string accountSID) {
-		this -> accountSID = accountSID;
-	}
-	string getAccountSID() {
-		return accountSID;
-	}
+//	public:
+	void setPhoneNumber(string phoneNumber);
+//		this ->phoneNumber = phoneNumber;
+//	}
+	string getPhoneNumber();
+	//	return phoneNumber;
+//	}
+	void setRole(string role);
+//		this ->role = role;
+//	}
+	string getRole();
+//		return role;
+//	}
+	void setAccountSID(string accountSID);
+//		this -> accountSID = accountSID;
+//	}
+	string getAccountSID();
+//		return accountSID;
+//	}
 	void setAuthToken(string authToken) {
-		this -> authToken = authToken;
-	}
-	string getAuthToken() {
-		return authToken;
-	}
-	void setTwilioNum(string twilioNum) {
-		this -> twilioNum = twilioNum;
-	}
-	string getTwilioNum() {
-		return twilioNum;
-	}
+//		this -> authToken = authToken;
+//	}
+	string getAuthToken();
+//		return authToken;
+//	}
+	void setTwilioNum(string twilioNum);
+///		this -> twilioNum = twilioNum;	}
+	string getTwilioNum(); 
+//		return twilioNum;
+//	}
+class Human {
+	string role, accountSID, authToken, twilioNum;
 };
+
+#endif
