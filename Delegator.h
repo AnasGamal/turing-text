23fc2319658ef01440c2b7b90e3389e1e40b2851
TC999999 <130585978+TC999999@users.noreply.github.com>
2023-04-26 22:41:54 -0400

    Create Delegator.h

---
+++ Delegator.h
@@ -0,0 +1,22 @@
+#ifndef DELEGATOR_H
+#define DELEGATOR_H
+
+#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+using namespace std;
+
+class Delegator{
+        queue<string> p_num;
+        queue<string> j_num;
+        set<string> asked_player_judge;
+        Gamebook* GB;
+        AI* ai;
+        Helptool* help;
+        TwilioClient* twilio;
+
+        public:
+        void analyze_request(string message, string number);
+        void pair_human_agent_evaluator(string p_num, string j_num);
+};
